1818c21d2a1c7eafbf0a44a7c4b96a3498536232
Kimral <121008986+Kimrral@users.noreply.github.com>
2024-09-07 22:43:30 +0900

    Fix : ScoreBoard Timer Logic Modify

--- Source/SpikySpike/UI/ScoreBoard.cpp
+++ Source/SpikySpike/UI/ScoreBoard.cpp
@@ -28,9 +28,18 @@ void UScoreBoard::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 
 void UScoreBoard::SetCountdownTimer()
 {
-	// Countdown 값을 현재 프레임의 델타 시간만큼 감소시킵니다.
-	// FMath::Clamp 함수를 사용하여 Countdown 값을 0.f에서 maxvalue 사이로 제한합니다.
-	Countdown = FMath::Clamp(Countdown -= GetWorld()->GetGameState()->GetServerWorldTimeSeconds(), 0.f, Countdown);
+	// // Countdown 값을 현재 프레임의 델타 시간만큼 감소시킵니다.
+	// // FMath::Clamp 함수를 사용하여 Countdown 값을 0.f에서 maxvalue 사이로 제한합니다.
+	// Countdown = FMath::Clamp(Countdown -= GetWorld()->GetGameState()->GetServerWorldTimeSeconds(), 0.f, Countdown);
+	//
+	// // Countdown 값을 문자열로 변환합니다.
+	// const FString CountdownString = UKismetStringLibrary::TimeSecondsToString(Countdown);
+ //    
+	// // 변환된 문자열을 CountdownValue 텍스트 위젯에 설정합니다.
+	// CountdownValue->SetText(FText::FromString(CountdownString));
+
+	// Countdown 값을 현재 프레임의 델타 시간(InDeltaTime)만큼 감소시킵니다.
+	Countdown = FMath::Clamp(Countdown - GetWorld()->GetDeltaSeconds(), 0.f, Countdown);
 
 	// Countdown 값을 문자열로 변환합니다.
 	const FString CountdownString = UKismetStringLibrary::TimeSecondsToString(Countdown);
