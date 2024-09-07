ec2054526e311b1393d938aa6bb3734a3d9711ea
Kimral <121008986+Kimrral@users.noreply.github.com>
2024-09-07 16:45:35 +0900

    Feat : Widget Assets & MainMenu, ScoreBoard Class Modify

--- Source/SpikySpike/UI/ScoreBoard.h
+++ Source/SpikySpike/UI/ScoreBoard.h
@@ -13,5 +13,31 @@ UCLASS()
 class SPIKYSPIKE_API UScoreBoard : public UUserWidget
 {
 	GENERATED_BODY()
+
+	
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+	void SetCountdownTimer();
+
+	// Timer Value Text Block
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
+	class UTextBlock* CountdownValue;
+
+	// First Team Score Text Block
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
+	class UTextBlock* FirstTeamScore;
+
+	// Second Team Score Text Block
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
+	class UTextBlock* SecondTeamScore;
+
+	// Appear Animation
+	UPROPERTY(BlueprintReadWrite, Transient, meta = (BindWidgetAnim))
+	class UWidgetAnimation* CountdownConstruct;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Countdown = 60.f;
+
 	
 };
